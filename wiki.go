bd32f260da2e96a5cbee2bfc39c73fbef7d3aee6
José Fernando Falcao <jfribeirofalcao@gmail.com>
2018-01-04 08:33:39 -0500

    Part2 complete

--- wiki.go
+++ wiki.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 )
 
 type Page struct {
@@ -24,9 +25,23 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<H1>%s</H1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	// part1
+	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page.")}
+	// p1.save()
+	// p2, _ := loadPage("TestPage")
+	// fmt.Println(string(p2.Body))
+
+	// Part2
+	// Call this page by typing http://localhost:8080/view/Testpage
+	// This will dispaly the content of TestPage.txt
+
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8080", nil)
 }
